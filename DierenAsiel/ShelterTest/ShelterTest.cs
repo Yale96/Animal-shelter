58c1b21710ef07173da4dca4c93763cc48fe8640
Leeuwen,Yannick Y.W.P. van <yannick.vanleeuwen@student.fontys.nl>
2016-03-14 18:31:23 +0100

    Unit tests eerste versie
    Eerste versie unit test bijgevoegd

---
+++ DierenAsiel/ShelterTest/ShelterTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DierenAsiel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShelterTest
+{
+    [TestClass]
+    public class ShelterTest
+    {
+        
+        [TestMethod]
+        public void TestAddAnimal()
+        {
+            Shelter shelter = new Shelter();
+            Animal animal = new Animal(55, "Yannick", 55, new Date(28, 09, 1996));
+            shelter.Add(animal);
+            Assert.AreEqual(1, shelter.Animals.Count);
+        }
+
+        [TestMethod]
+        public void TestAddAnimalWichIsNull()
+        {
+            Shelter shelter = new Shelter();
+            Animal animal = null;
+            shelter.Add(animal);
+            Assert.AreEqual(0, shelter.Animals.Count);
+        }
+    }
+}
