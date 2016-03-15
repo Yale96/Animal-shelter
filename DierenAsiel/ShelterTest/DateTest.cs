0360c684b35ed8e5b7af16db957e52d4b3e8be8e
Yale96 <yannickvanleeuwen@i-lion.nl>
2016-03-15 14:28:31 +0100

    Tests toegevoegd

---
+++ DierenAsiel/ShelterTest/DateTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DierenAsiel;
+
+namespace ShelterTest
+{
+    [TestClass]
+    public class DateTest
+    {
+        [TestMethod]
+        public void TestCreateDate()
+        {
+            Date date = new Date(28, 09, 1996);
+            Assert.AreEqual(date.Day, 28);
+            Assert.AreEqual(date.Month, 09);
+            Assert.AreEqual(date.Year, 1996);
+        }
+
+        [TestMethod]
+        public void TestCreateDateInFuture()
+        {
+            Date date = new Date(28, 09, 2025);
+            Assert.AreEqual(date.Day, 28);
+            Assert.AreEqual(date.Month, 09);
+            Assert.AreEqual(date.Year, 2025);
+        }
+    }
+}
---
+++ DierenAsiel/ShelterTest/DogTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DierenAsiel;
+
+namespace ShelterTest
+{
+    [TestClass]
+    public class DogTest
+    {
+        [TestMethod]
+        public void TestCreateDog()
+        {
+            Date DateOfBirth = new Date(28, 09, 1996);
+            Date LastWalkDate = new Date(15, 03, 2016);
+            Dog dog = new Dog(55, "henk", 44, DateOfBirth, LastWalkDate);
+            Assert.AreEqual(dog.RegistrationNumber, 55);
+            Assert.AreEqual(dog.Name, "henk");
+            Assert.AreEqual(dog.Price, 44);
+            Assert.AreEqual(dog.DateOfBirth, DateOfBirth);
+            Assert.AreEqual(dog.LastWalkDate, LastWalkDate);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void TestCreateDogWichIsNull()
+        {
+            Dog dog = null;
+            Assert.AreEqual(dog.RegistrationNumber, null);
+            Assert.AreEqual(dog.Name, null);
+            Assert.AreEqual(dog.Price, null);
+            Assert.AreEqual(dog.DateOfBirth, null);
+            Assert.AreEqual(dog.LastWalkDate, null);
+        }
+    }
+}
