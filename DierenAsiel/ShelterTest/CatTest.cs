f4451e003fd2bcbf8102b812cd707ecc4dc6e5bc
Leeuwen,Yannick Y.W.P. van <yannick.vanleeuwen@student.fontys.nl>
2016-03-17 17:09:21 +0100

    CatTest klasse toegevoegd

--- DierenAsiel/ShelterTest/CatTest.cs
+++ DierenAsiel/ShelterTest/CatTest.cs
@@ -1,13 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ShelterTest
 {
-    class CatTest
+    [TestClass]
+    public class CatTest
     {
+        [TestMethod]
+        public void TestMethod1()
+        {
 
+        }
     }
 }
