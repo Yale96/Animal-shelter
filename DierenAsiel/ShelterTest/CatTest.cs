26751cc8fcf28673c5e62dde5a9de1dcf1898f42
Leeuwen,Yannick Y.W.P. van <yannick.vanleeuwen@student.fontys.nl>
2016-03-17 17:17:49 +0100

    Alle tests toegevoegd

--- DierenAsiel/ShelterTest/CatTest.cs
+++ DierenAsiel/ShelterTest/CatTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DierenAsiel;
 
 namespace ShelterTest
 {
@@ -7,9 +8,27 @@ namespace ShelterTest
     public class CatTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCreateCat()
         {
+            Date dateOfBirth = new Date(28, 09, 1996);
+            Cat cat = new Cat(25, "Yannick", 25, dateOfBirth, "Is net nieuw in het asiel");
+            Assert.AreEqual(cat.RegistrationNumber, 25);
+            Assert.AreEqual(cat.Name, "Yannick");
+            Assert.AreEqual(cat.Price, 25);
+            Assert.AreEqual(cat.DateOfBirth, dateOfBirth);
+            Assert.AreEqual(cat.Information, "Is net nieuw in het asiel");
+        }
 
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void TestCreateCatWichIsNull()
+        {
+            Cat cat = null;
+            Assert.AreEqual(cat.RegistrationNumber, null);
+            Assert.AreEqual(cat.Name, null);
+            Assert.AreEqual(cat.Price, null);
+            Assert.AreEqual(cat.DateOfBirth, null);
+            Assert.AreEqual(cat.Information, null);
         }
     }
 }
